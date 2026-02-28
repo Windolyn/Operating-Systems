62adb9a7d06d980b215995b2f8f1f80f66b2aaa5
GwenKhou <gwendolynkhou0311@gmail.com>
2026-02-27 23:21:12 -0600

    update last inshAllah

--- my-sum.cpp
+++ my-sum.cpp
@@ -1,27 +1,26 @@
-#include <stdio.h>  //for i/o functions
-#include <stdlib.h> //for atoi, exit, etc
-#include <sys/types.h> //for pid_t
-#include <unistd.h> //for fork()
-#include <sys/ipc.h> //for shared memory
-#include <sys/shm.h> //for shared memory
-#include <sys/wait.h> //for wait
-#include <math.h> //for log2 and ciel
+#include <stdio.h>  
+#include <stdlib.h>
+#include <sys/types.h> 
+#include <unistd.h> 
+#include <sys/ipc.h> 
+#include <sys/shm.h> 
+#include <sys/wait.h> 
+#include <math.h> 
 
 
 
 /*components of this program:
 1. Main Program to run 
+    - Worker processes to compute sums in parallel
+    - Parent process to write output
 2. Barrier Structure in o(1) space and reusable
-3. Worker processes to compute sums in parallel
-4. Parent process to write output
 
 */
 
-//barrier logic, each worker sets a flag, and waits until all flags are set before moving on
+//reuseable barrier w/ flags 
 void barrier(int *flags, int w, int m, int phase){
-//flags is an array of size m, where each worker sets its flag to indicate completion of a phase. 
 //w is the current worker, m = total # of workers, phase is round of computation 
-    flags[w] = phase + 1; // signals the completion 
+    flags[w] = phase + 1; 
 
     int finished;
     do{
@@ -33,142 +32,165 @@ void barrier(int *flags, int w, int m, int phase){
                 break;
             }
         }
-        if(!finished) usleep(10); //sleep for a bit to avoid busy waiting
+        if(!finished) usleep(10); 
     } while (!finished);
     
 }
-//end of barrier function
 
 
-//main function
+
+
+
+
+
+//main function, # of args and array of args
 int main(int argc, char *argv[]) {
-    if (argc != 5) { //takes in program name and 4 arguments | n (#elements), m(#workers), inputfile and outputfile
+    if (argc != 5) { 
         printf("Usage: %s n m input.txt output.txt\n", argv[0]);
         return 1;
     }
 
 
     //converitng arguments from strings to integers
-    long long n = atoi(argv[1]); //# elements
-    long long m = atoi(argv[2]); //# workers
+    long long n = atoi(argv[1]);
+    long long m = atoi(argv[2]); 
 
-    if(n <= 0 || m <= 0 || m > n){ //sanity check
+    if(n <= 0 || m <= 0 || m > n){ 
         printf("Invalid arguments, you must have a positive n and m, and n > m\n");
         return 1; 
     }
 
 
 
-    char *inputFile = argv[3]; // storing inputfile name
-    char *outputFile = argv[4]; //stores outputfile
+    char *inputFile = argv[3]; 
+    char *outputFile = argv[4];
 
-    FILE *in = fopen(inputFile, "r"); //open inputfile to read
-    if(!in){    //if you don't make it in, print and dip
+    FILE *in = fopen(inputFile, "r"); 
+    if(!in){    
         perror("error opening input file");
         return 1;
 
     }
 
-    int shmid = shmget(IPC_PRIVATE, (2*n + 1 + m) * sizeof(long long), IPC_CREAT | 0666);  //craete shared memory segment
-    //holds two buffers size n, selector for current buffer, and m flags barrier flags
-    long long *shared = (long long *) shmat(shmid, NULL, 0); //give pointer to this shared memory
+    int shmid = shmget(IPC_PRIVATE, (2*n + 1 + m) * sizeof(long long), IPC_CREAT | 0666);  //craete shared memory 
+    
+    long long *shared = (long long *) shmat(shmid, NULL, 0); 
 
-    long long *A = shared;  //worker 1
-    long long *B = shared + n; //worker 2
-    long long *current = shared + 2*n; //current buffer selector
-    int *flags = (int *)(shared + 2*n + 1); //barrier flags
-    *current = 0; //current buffer to be 0, so starts at buffer a
+    long long *A = shared;  
+    long long *B = shared + n; 
+    long long *current = shared + 2*n;
+    int *flags = (int *)(shared + 2*n + 1);
+    *current = 0;
 
-    for(int i = 0; i < m; i++) flags[i] = 0; //initializing flags to be 0
+    for(int i = 0; i < m; i++) flags[i] = 0; 
 
     for (int i = 0; i < n; i++){
-        if(fscanf(in, "%lld\n", &A[i]) != 1){ //read in input file, if not enough elements, print and exit
+        if(fscanf(in, "%lld\n", &A[i]) != 1){ 
             printf("input file does not contain enough elements\n");
             fclose(in);
             return 1;
         }
-    } fclose(in);  //close the input file
+    } 
+    
+    
+    fclose(in); 
 
 
-    //spawn workers process
-    int rounds = (int) ceil(log2(n)); //# rounfs of computation needed to compute sum
-    //spawn workers                   //each rounds halfs the # of active workeers
+
+
+    //workers process
+    int rounds = (int) ceil(log2(n)); 
+
+
+
     for(int w = 0; w < m; w++){
         pid_t pid = fork();   //create prrocess
-        if(pid < 0){    //if the fork fails, exit
+        if(pid < 0){   
             perror("Forking failed");
             return 1;
         }
 
          //child process
         if(pid == 0){
-            int chunk = n/m;    //segmenting a piece for each worker
-            int remainder = n % m;  //incase n is not divisible by m, distribute rest of elements
-            int start = w * chunk + (w < remainder ? w : remainder); //starting indec for w worker
-            int end = start + chunk + (w < remainder ? 1 : 0); //ending index for w worker
 
-            //looping through the rounds, each round halves w & doubles step size
+            int chunk = n/m;  
+            int remainder = n % m; 
+            int start = w * chunk + (w < remainder ? w : remainder); 
+            int end = start + chunk + (w < remainder ? 1 : 0); 
+
+
             for(int p = 0; p < rounds; p++){
-                int step = 1 << p;  //how far apart elements to the sum are, doubles every round
-                long long *source = (*current == 0) ? A : B;   //source buffer in memory
-                long long *destination = (*current == 0) ? B : A;   //destinartion buffer in memory
+                int step = 1 << p;  
+                long long *source = (*current == 0) ? A : B;  
+                long long *destination = (*current == 0) ? B : A;   
 
 
 
                 //prefix sum
                 for(int i = start; i < end; i++){
                 
-                    //if i > step, sum current element with element a step behind
+                   
                     if(i >= step) 
                 
                         destination[i] = source[i] + source[i - step];   
                     
                     else
-                        destination[i] = source[i];     //else copy the element
+                
+
+                        destination[i] = source[i];    
 
                 }
 
 
-                barrier(flags, w, m, p);     //call barrier to wait for workers
+                barrier(flags, w, m, p);     
                 
-                if(w == 0) *current = 1 - *current;  //protect against race condition, only w0 changes barrier
+            
+
+                if(w == 0) *current = 1 - *current;  // w0 changes barrier
 
 
-                barrier(flags, w, m, p + rounds);   //barrier switch before next round
+                barrier(flags, w, m, p + rounds);   
 
 
             
         }
 
-            shmdt(shared);    //remove from shared memory
-            exit(0);   //exit child process
+            shmdt(shared);   
+            exit(0);   
 
 
     }
+
+
+
 }
 
 
 
-    //wait for workers finish
+    
     for(int w = 0; w < m; w++) 
         wait(NULL); 
 
 
     //output file
-    FILE *out = fopen(outputFile, "w");   //open outputFile
-    long long *answers = (*current == 0) ? A : B;    //pointer to final answer buffer
+    FILE *out = fopen(outputFile, "w");  
+    long long *answers = (*current == 0) ? A : B;    
 
         for(int i = 0; i < n; i++){
             fprintf(out, "%lld ", answers[i]);    //write to the utputFile
 
         }
+
+
+
+        //cleaning up et closing out
         fprintf(out, "\n");   
-        fclose(out);   //close output file
+        fclose(out);   
 
         printf("output written to Output file \n");
 
-        shmdt(shared);    //remove from shared memory
-        shmctl(shmid, IPC_RMID, NULL);    //shared memory for deletion
+        shmdt(shared);    
+        shmctl(shmid, IPC_RMID, NULL);   
    
 
 
